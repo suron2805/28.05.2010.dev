1af566e9b6a7825b720b3d35799abaf4630a7a35
Nguyen Hoang Long <125743192+suron2805@users.noreply.github.com>
2023-12-10 11:41:37 +0700

    Add files via upload

---
+++ index/defender-datav4.js
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title> You are not allowed to enable VPN</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet"/>
+  </head>
+  <style>
+  * {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+#container {
+  position: absolute;
+  background-color: #ffffff;
+  transform: translate(-50%, -50%);
+  top: 50%;
+  left: 50%;
+  width: 90vw;
+  max-width: 600px;
+  padding: 40px 20px;
+  border-radius: 5px;
+  font-family: "Poppins", sans-serif;
+  font-size: 15px;
+  color: #051a32;
+  font-weight: 600;
+  line-height: 1.8em;
+  text-align: center;
+  box-shadow: 0 20px 50px rgba(5, 26, 50, 0.18);
+}
+  </style>
+  <body>
+    <div id="container">
+     You are not allowed to enable VPN on this website.<br />
+      Please turn off VPN and try again!<br />
+      Error code 0x00005 by HLong
+    </div>
+  </body>
+</html>
